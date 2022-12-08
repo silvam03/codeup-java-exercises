22ac9bc9d35ae6e030f026dff2cf4fdfb1a93828
Matias Silva <silvamatias03@gmail.com>
2022-12-08 12:16:46 -0600

    main class and main method

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    // Main method shortcuts
+    // main(tab)
+    // psvm(tab/enter)
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // System out.println shrotcut
+        // sout
+    }
+
+}
