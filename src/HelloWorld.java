f9a528575132f2b2102cf65f3e96620874a3479f
Matias Silva <silvamatias03@gmail.com>
2022-12-08 16:39:31 -0600

    Syntax, Types, and Variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,53 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string value";
+        System.out.println(myString);
+
+//        long MyNumber = 3.14;
+//        System.out.println(MyNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int class;
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+//        System.out.println(three);
+
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        y += y * x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//
+//        y -= x;
+//        System.out.println(y);
+
+
     }
 
 }
