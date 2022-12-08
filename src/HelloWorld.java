2458477fc193c388e3fa5c6b0869ffd623d43cf3
Matias Silva <silvamatias03@gmail.com>
2022-12-08 15:01:48 -0600

    Intro to java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("Hello world!");
-        System.out.print("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
-    
+
 }
