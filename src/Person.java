49b1391c38f63c2ce0ff2a9a68641e6d8d6b6538
Matias Silva <silvamatias03@gmail.com>
2023-01-10 12:33:54 -0600

    Completed Person.java Exercise

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+public class Person {
+    private String name;
+    public Person(String name) {
+        this.name = name;
+    }
+
+
+    public static void main(String[] args) {
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        person1.setName("Bill");
+//        System.out.println(person1 == person2);
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+    }
+
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello" + this.name);
+    }
+
+}
